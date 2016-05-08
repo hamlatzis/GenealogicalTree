cc74e59f587ff75cf29bc8ed5f81b401396a8bab
iosif hamlatzis <i.hamlatzis@gmail.com>
2016-05-08 14:18:41 +0300

    Initial call (experiment) to graph

--- GenealogicalTree.cpp
+++ GenealogicalTree.cpp
@@ -3,9 +3,26 @@
 
 #include "stdafx.h"
 
+#include "GenGraph.h"
+#include "Person.h"
+
+#include <vector>
+#include <string>
+#include <assert.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::vector<GenGraph::branch> vBranches;
+	std::string strDescendantName = "";
+	std::string strAscendantName = "";
+
+	GenGraph genGraph;
+	bool bFound = genGraph.GetFamilyTree( vBranches, strDescendantName, strAscendantName );
+	if ( bFound )
+	{
+		assert ( vBranches.empty() == false );
+	}
+
 	return 0;
 }
 
